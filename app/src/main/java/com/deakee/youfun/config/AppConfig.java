4f7d6bed66b5e572419e0b430190466375eea94a
Deekea <274582643@qq.com>
2015-02-21 21:27:00 +0800

    添加item_main_topic
    添加item_main_topic

---
+++ app/src/main/java/com/deakee/youfun/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.deakee.youfun.config;
+
+/**
+ * Created by Deekea on 2015/2/21.
+ */
+public class AppConfig {
+}
---
+++ app/src/main/java/com/deakee/youfun/config/YouFunApplication.java
@@ -0,0 +1,10 @@
+package com.deakee.youfun.config;
+
+import android.app.Application;
+
+/**
+ * Created by Deekea on 2015/2/21.
+ */
+public class YouFunApplication extends Application{
+
+}
