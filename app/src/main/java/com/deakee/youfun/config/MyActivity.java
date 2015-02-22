b9370d83dc60abf4f72f12feeba6f08fa5db3626
招振涛 <514656282@qq.com>
2015-02-22 09:41:30 +0800

    add pic by zhaozhentao

--- app/src/main/java/com/deakee/youfun/config/MyActivity.java
+++ app/src/main/java/com/deakee/youfun/config/MyActivity.java
@@ -1,7 +1,9 @@
 package com.deakee.youfun.config;
 
+import android.support.v7.app.ActionBarActivity;
+
 /**
  * Created by zzt on 2015/2/21.
  */
-public class MyActivity {
+public class MyActivity extends ActionBarActivity {
 }
